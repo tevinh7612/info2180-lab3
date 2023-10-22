3ea12eb3d93ba0fef98cb5dc24e78008dbbf4900
Tevin Hislop <tevin.hislop@mymona.uwi.edu>
2023-10-22 15:34:57 -0500

    Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';
+    let board = ['', '', '', '', '', '', '', '', ''];
   
     squares.forEach((square, index) => {
       square.classList.add('square', 'empty');
       square.dataset.index = index;
+
+      square.addEventListener('click', () => {
+        if(square.classList.contains('empty')){
+            square.classList.remove('empty');
+            square.classList.add(currentPlayer);
+            square.textContent = currentPlayer;
+            board[index] = currentPlayer;
+
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+            }
+        });
     });
   });
